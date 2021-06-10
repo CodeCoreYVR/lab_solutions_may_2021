e616fd0d21db7e30b70e1f6fefcc7b0e65ae8472
Mao Li <anrenyil@gmail.com>
2021-06-09 17:00:58 -0700

    js constructors and prototype

---
+++ js_constructors_prototype/library.js
@@ -0,0 +1,26 @@
+class Library {
+    constructor() {
+        this.bookList = [];
+    }
+    shelve(book) {
+        this.bookList.push(book);
+        return this;
+    }
+    findByTitle(name) {
+        for (const item of this.bookList) {
+            if (item.title === name) {
+                return item;
+            }
+        }
+    }
+    list() {
+        return this.bookList;
+    }
+}
+class Book {
+    constructor(title, authors, edition) {
+        this.title = title;
+        this.authors = authors;
+        this.edition = edition;
+    }
+}
