dbdf26e9918be11eb56af893894698dce9660ed7
Mao Li <anrenyil@gmail.com>
2021-06-24 16:11:38 -0700

    express midware and cookies

--- js_express_midware_and_cookies/app.js
+++ js_express_midware_and_cookies/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-
+const tasksRouter = require('./routes/tasks');
 const app = express();
+
+
 app.use(logger('dev'));
 app.set('view engine', 'ejs');
 
@@ -53,6 +55,24 @@ app.post('/preferred_language', function (request, response) {
     response.redirect('/');
 });
 
+
+// Custom middleware
+app.use((request, response, next) => {
+    const todoList = request.cookies.todoList;
+    response.locals.todoList = [];
+    if (todoList) {
+        // This initializes a variable called `todoList` which will be available to
+        // all of the views. The variable `todoList` is assigned the value of the
+        // `cookies.todoList` array
+        response.locals.todoList = todoList;
+    }
+
+    // The middleware is finished. Go on to next middleware and/or route
+    next();
+});
+
+
+app.use(tasksRouter);
 const PORT = 5000;
 const DOMAIN = 'localhost';
 app.listen(PORT, DOMAIN, function () {
