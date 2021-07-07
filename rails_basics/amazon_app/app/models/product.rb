514d4da5a29f8b66814a4ae0be8a10cc2b444a3a
Mao Li <anrenyil@gmail.com>
2021-07-07 16:11:01 -0700

    rails active record

---
+++ rails_basics/amazon_app/app/models/product.rb
@@ -0,0 +1,25 @@
+class Product < ApplicationRecord
+    after_initialize :set_default_price
+    before_save :capitalize_title
+    # The title must be present
+    # The title must be unique (case insensitive)
+    # The price must be a number that is more than 0
+    # The description must be present
+    # The description must have at least 10 characters
+    validates :title, presence: true, uniqueness: { case_sensitive: false }
+    validates :price, numericality:{greater_than: 0}
+    validates :description, presence: true, length: { minimum: 10 }
+
+    # A callback method to set the default price to 1
+    # A callback method to capitalize the product title before saving
+    private
+    def set_default_price
+        self.price ||= 1
+    end
+
+    def capitalize_title
+        self.title.capitalize!
+    end
+    
+
+end
--- rails_basics/amazon_app/db/seeds.rb
+++ rails_basics/amazon_app/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+1000.times do
+    created_time = Faker::Date.between(from: '2000-09-23', to: '2030-09-25')
+    Product.create(
+        title: Faker::FunnyName.name,
+        description: Faker::Restaurant.name,
+        created_at: created_time,
+        updated_at: created_time
+    )
+end
+
+list = Product.all
+
+puts list.count
