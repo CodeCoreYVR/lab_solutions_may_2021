fb1e586bf0aa33d9f1753ccef7a547eed5aee72b
Mao Li <anrenyil@gmail.com>
2021-08-03 16:03:08 -0700

    js spa edit and delete

--- js_spa/main.js
+++ js_spa/main.js
@@ -37,6 +37,25 @@ const Product = {
             },
             body: JSON.stringify(params)
         }).then(res => res.json());
+    },
+    update(id, params) {
+        return fetch(`${BASE_URL}/products/${id}`, {
+            credentials: "include",
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(params)
+        }).then(res => res.json());
+    },
+    delete(id) {
+        return fetch(`${BASE_URL}/products/${id}`, {
+            credentials: "include",
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res => res.json());
     }
 };
 
@@ -66,6 +85,8 @@ function getAndDisplayProduct(id) {
             <p>${product.description}</p>
             <small>Price: ${product.price}</small>
             <a class="link" data-target="product-edit" data-id="${product.id}" href="">Edit</a>
+            |
+            <a class="link" data-target="product-delete" data-id="${product.id}" href="">Delete</a>
             <h3>Reviews</h3>
             <ul>
                 ${product.reviews.map(a => '<li>' + a.body + '</li>').join("")}
@@ -89,6 +110,15 @@ function navigateTo(sectionId) {
     });
     document.querySelector(`.navbar [data-target=${sectionId}]`).classList.add("active");
 
+}
+
+function setupForm(id) {
+    Product.one(id).then(product => {
+        document.querySelector("#edit-product-form [name=title]").value = product.title;
+        document.querySelector("#edit-product-form [name=description]").value = product.description;
+        document.querySelector("#edit-product-form [name=price]").value = product.price;
+        document.querySelector("#edit-product-form [name=id]").value = product.id;
+    });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -126,6 +156,40 @@ document.addEventListener("DOMContentLoaded", () => {
         };
         Product.create(newProduct).then(product => {
             newProductForm.reset();
+            getAndDisplayProduct(product.id);
+        });
+    });
+
+    document.querySelector("#product-show").addEventListener("click", event => {
+        const link = event.target.closest("[data-target]");
+        if (link) {
+            const targetPage = link.getAttribute("data-target");
+            const id = link.getAttribute("data-id");
+            event.preventDefault();
+            if (targetPage.indexOf('delete') > -1) {
+                Product.delete(id).then(res => {
+                    navigateTo("product-index");
+                });
+            } else {
+                setupForm(id);
+                navigateTo(targetPage);
+            }
+        }
+    });
+
+    const editProductForm = document.querySelector("#edit-product-form");
+    editProductForm.addEventListener("submit", event => {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        const updatedProduct = {
+            title: formData.get("title"),
+            description: formData.get("description"),
+            price: formData.get("price")
+        };
+
+        Product.update(formData.get("id"), updatedProduct).then(product => {
+            editProductForm.reset();
             getAndDisplayProduct(product.id);
         });
     });
