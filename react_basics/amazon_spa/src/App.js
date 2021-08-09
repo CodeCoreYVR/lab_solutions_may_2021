849e9f2d271545195b5492b7971e0c656a323c90
Mao Li <anrenyil@gmail.com>
2021-08-09 16:03:14 -0700

    react basics

---
+++ react_basics/amazon_spa/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ProductShowPage from './components/ProductShowPage'
+
+function App() {
+  return (
+    <div >
+      <ProductShowPage />
+    </div>
+  );
+}
+
+export default App;
